845c03e4447bd6f594d4be502b8eb165778999e5
prajwalnehe <prajwalnehe383@gmail.com>
2025-11-17 18:21:42 +0530

    logo commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
             alt: 'Festive Offer - SareeSansaar',
           },
         ]}
-        mobileSrc="https://res.cloudinary.com/duc9svg7w/image/upload/v1762318223/Beige_And_Brown_Luxury_Shopping_Guide_Instagram_Post_20251104_225913_0000_tbyvtj.png"
+        mobileSrc="https://res.cloudinary.com/duc9svg7w/image/upload/v1763383307/file_0000000032ac7209890b93ba8217ac10_1_nfz2u2.png"
       />
 
       {/* Shop By Gender */}
