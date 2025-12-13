e41e3a57be8b9492d39d710cebd163bf1e1f4318
prajwalnehe <prajwalnehe383@gmail.com>
2025-12-13 15:39:34 +0530

    done

--- frontend/src/components/FeaturedProducts.jsx
+++ frontend/src/components/FeaturedProducts.jsx
@@ -98,12 +98,10 @@ const FeaturedProducts = ({ category = 'shirts', layout = 'scroll', maxProducts 
 
   if (loading) {
     return (
-      <section className="py-16 bg-white">
+      <section className="py-8 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex gap-4 overflow-x-auto">
-            {[...Array(4)].map((_, i) => (
-              <div key={i} className="flex-shrink-0 w-[280px] bg-gray-200 animate-pulse h-96 rounded"></div>
-            ))}
+          <div className="flex justify-center items-center py-12">
+            <div className="loader"></div>
           </div>
         </div>
       </section>
